508c1464c4f9822e62aa8332411f7b0c8e993b53
Henk <hbokhoven@gmail.com>
2021-11-30 12:33:32 +0100

    Vim recharged

--- run_once_after_90-vim-setup.sh
+++ run_once_after_90-vim-setup.sh
@@ -4,9 +4,5 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 if [ -z "$(ls -A ~/.vim/bundle)" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
-    # git clone https://github.com/dracula/vim.git ~/.vim/bundle/dracula
+  vim -c PluginUpdate -c qa
 fi
-
-vim -c PluginUpdate -c qa
