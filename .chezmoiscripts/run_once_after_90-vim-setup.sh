c4641a6a4716de43d24f291cc87ac1522fbbef19
Henk <hbokhoven@gmail.com>
2021-12-01 14:58:39 +0100

    Add dotfiles

--- .chezmoiscripts/run_once_after_90-vim-setup.sh
+++ .chezmoiscripts/run_once_after_90-vim-setup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-if [ -z "$(ls -A ~/.vim/bundle/Vundle.vim)" ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
 # Update all plugins set in ~/.vimrc
-vim -c PluginUpdate -c qa
+vim -c PlugInstall -c qa
