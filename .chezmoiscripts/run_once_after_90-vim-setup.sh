e1a7cb683a71ed970ff50b768e1b77e7a95b4da6
Henk <hbokhoven@gmail.com>
2021-12-01 14:29:50 +0100

    Add .chezmoiexternal.toml

--- .chezmoiscripts/run_once_after_90-vim-setup.sh
+++ .chezmoiscripts/run_once_after_90-vim-setup.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-if [ -z "$(ls -A ~/.vim/bundle)" ]; then
+if [ -z "$(ls -A ~/.vim/bundle/Vundle.vim)" ]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
+# Update all plugins set in ~/.vimrc
 vim -c PluginUpdate -c qa
