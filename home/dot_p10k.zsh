89a9a9ba723f3800d102acd2df19dd16efc3b5ab
Henk <hbokhoven@gmail.com>
2023-11-30 13:18:13 +0100

    chore: nitpick on dot_p10k.zsh

--- home/dot_p10k.zsh
+++ home/dot_p10k.zsh
@@ -25,12 +25,12 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    os_icon               # os identifier
+    os_icon                 # os identifier
     dir                     # current directory
     vcs                     # git status
     # =========================[ Line #2 ]=========================
     newline                 # \n
-    prompt_char           # prompt symbol
+    prompt_char             # prompt symbol
   )
 
   # The list of segments shown on the right. Fill it with less important segments.
