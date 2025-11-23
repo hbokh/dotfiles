68d45f15551b55179ed66f4e1a70a9aa39faaeb6
Henk <hbokhoven+github@gmail.com>
2025-11-23 12:09:22 +0100

    chore: add rfv function

--- home/exact_dot_oh-my-zsh/exact_custom/my-functions.zsh
+++ home/exact_dot_oh-my-zsh/exact_custom/my-functions.zsh
@@ -1,6 +1,6 @@
 # Functions
 
-# kube-ps1 like prompt ##########################################
+## kube-ps1 like prompt ##########################################
 
 function kube-toggle() {
   if (( ${+POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND} )); then
@@ -32,7 +32,27 @@ function lctl {
     fi
 }
 
-# https://www.reddit.com/r/MacOS/comments/zq36l1/whats_up_with_mdfind_warning_on_console/
+## https://www.reddit.com/r/MacOS/comments/zq36l1/whats_up_with_mdfind_warning_on_console/
 function mdfind() {
     /usr/bin/mdfind $@ 2> >(grep --invert-match ' \[UserQueryParser\] ' >&2)
+}
+
+## ripgrep->fzf->vim [QUERY]
+## https://junegunn.github.io/fzf/tips/ripgrep-integration/
+function rfv() {
+  RELOAD='reload:rg --column --color=always --smart-case {q} || :'
+  OPENER='if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
+            vim {1} +{2}     # No selection. Open the current line in Vim.
+          else
+            vim +cw -q {+f}  # Build quickfix list for the selected items.
+          fi'
+  fzf --disabled --ansi --multi \
+      --bind "start:$RELOAD" --bind "change:$RELOAD" \
+      --bind "enter:become:$OPENER" \
+      --bind "ctrl-o:execute:$OPENER" \
+      --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+      --delimiter : \
+      --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+      --preview-window '~4,+{2}+4/3,<80(up)' \
+      --query "$*"
 }
